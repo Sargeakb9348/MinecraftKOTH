8132c501d653f6a5fb8d8831a30391b47dc300df
Kyle Sargeant <Sargeakb9348@uwec.edu>
2022-08-08 19:10:11 -0500

    initial skeletal code for surverutil class and uuidhelper class. TODO: update out of date packages

--- src/main/java/com/kybit/minecraftkoth/command/utils/UUIDHelper.java
+++ src/main/java/com/kybit/minecraftkoth/command/utils/UUIDHelper.java
@@ -1,4 +1,43 @@
 package com.kybit.minecraftkoth.command.utils;
 
+import java.util.UUID;
+import com.kybit.minecraftkoth.utils.ServerUtils;
+import com.mojang.authlib.GameProfile;
+
+/**
+ * Provides the capacity to switch between the name and the UUID of a player.
+ *
+ * @author BrokenSwing
+ *
+ */
 public class UUIDHelper {
+
+    /**
+     * Retrieves the {@link UUID} of a player from hs name.
+     *
+     * @param playerName
+     *            The name of the player
+     * @return the UUID of the player
+     */
+    public static UUID getUUIDOf(final String playerName) {
+        final GameProfile profile = ServerUtils.getServer().getPlayerProfileCache().getGameProfileForUsername(playerName);
+        if (profile != null)
+            return profile.getId();
+        return null;
+    }
+
+    /**
+     * Retrieves the name of a player from his {@link UUID}.
+     *
+     * @param uuid
+     *            The UUID of the player
+     * @return the name of the player
+     */
+    public static String getNameOf(final UUID uuid) {
+        final GameProfile profile = ServerUtils.getServer().getPlayerProfileCache().getProfileByUUID(uuid);
+        if (profile != null)
+            return profile.getName();
+        return null;
+    }
+
 }
